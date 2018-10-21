4ff11996f436aa3dce1417b972a00cff9ff0a373
Mohammad Naser Abbasabadi <mn_abbasabadi@yahoo.com>
2018-10-22 00:02:14 +0330

    MnAbbas

--- middlewares/404.js
+++ middlewares/404.js
@@ -7,7 +7,7 @@ module.exports =  function( req, res, next) {
   // var err = new Error('Not Found');
   // err.status = 404;
   res.status(404).header("Content-Type", "application/json").send(JSON.stringify({
-    error : " Path is not founded"
+    error : "PATH_NOT_FOUNDED"
   }));
 
 };
